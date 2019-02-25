dadd08aded4ab4654ab3f1e6cadaaff5e815230c
fouadpy <perceval@protonmail.com>
2019-02-25 23:38:54 +0100

    Update home page animation and redirection user login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    # if current_user
-    #   redirect_to books_path
-    # end
+    redirect_to books_path if current_user
     @books = Book.last(4)
   end
 end
