11b05a38d420594b4fd1a0ee699a58ef730dd453
fouadpy <perceval@protonmail.com>
2019-02-26 17:10:28 +0100

    Added layout for homepage and fix book view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
-
+  layout 'home'
   def home
     redirect_to books_path if current_user
     @books = Book.last(4)
