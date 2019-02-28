d2b2626c053e916a0faa3112a0c49ebd9c8be72c
fouadpy <perceval@protonmail.com>
2019-02-28 12:44:29 +0100

    Added (friendly_id) slugs to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -62,7 +62,7 @@ class BooksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
-      @book = Book.find(params[:id])
+      @book = Book.friendly.find(params[:id])
       authorize @book
     end
 
