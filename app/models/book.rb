68350a3377965254840c8b6943149a5e52cfceaa
fouadpy <perceval@protonmail.com>
2019-02-28 13:01:15 +0100

    Added sale model

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   friendly_id :name, use: :slugged
 
   belongs_to :user
+  has_many :sales
   has_attached_file :image
   has_attached_file :resource
 
@@ -15,4 +16,7 @@ class Book < ApplicationRecord
   message: "Only pdfs allowed"
 
   validates :image, attachment_presence: true
+
+  validates_numericality_of :price,
+  greater_than: 49, message: "must be at least 50 cents"
 end
